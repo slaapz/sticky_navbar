8430545f1f727a0b55366509e8380c43beaf4925
Patrick Ball <slaapz@gmail.com>
2023-03-16 21:46:37 -0600

    Added sticky functionality to the navbar when scrolling

---
+++ sticky.js
@@ -0,0 +1,12 @@
+const navbarEl = document.querySelector('.navbar');
+const bottomContainerEl = document.querySelector('.bottom-container');
+
+console.log(navbarEl.offsetHeight)
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50) {
+        navbarEl.classList.add('active');
+    } else {
+        navbarEl.classList.remove('active');
+    }
+});
